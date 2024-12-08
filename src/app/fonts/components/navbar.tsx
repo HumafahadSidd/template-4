d9eb26959d5ea132f84f2a60c20be148dcc5d43b
HumafahadSidde <fhuma302@gmail.com>
2024-12-08 22:40:15 +0500

    Initial commit from Create Next App

---
+++ src/app/fonts/components/navbar.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <div>
+        <div>
+            <ul>
+                <li>Ecommerce Acceories & Fashion item </li>
+                <li>Per Page:</li>
+                <li>Per Page:</li>
+                <li></li>
+            </ul>
+            </div> 
+    </div>
+  )
+}
